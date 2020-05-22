92b56780c2dfb92def106df2a552f7671b284b5f
Gerald Bivens <geraldbivens@gmail.com>
2020-05-22 16:47:40 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_value = 1000
+  min_value_key = nil
   name_hash.each do |key_name, number|
     if min_value > number
       min_value = number
+      min_value_key = key_name
+    end
   end
-  # How do I return the key associated with this value?
+  min_value_key
 end
